1f5610ef8eb786413321e30cd6963e86028a595f
John Boesen <jmboesen@college.harvard.edu>
2023-06-23 15:48:19 -0500

    FIX notebook cleanup + rm tf

--- tcellmatch/api/utils.py
+++
@@ -1,3 +0,0 @@
-from tcellmatch.estimators import deviation_label, deviation_global, auc_label, auc_global, pr_label, pr_global
-from tcellmatch.utils.resample import balance_multiclass_dataset
-# from tcellmatch.utils import visualize, resample
--- tcellmatch/estimators/__init__.py
+++
@@ -1,4 +0,0 @@
-from .estimator_base import EstimatorBase
-from .estimator_binary import EstimatorBinary
-from .estimator_ffn import EstimatorFfn
-from .additional_metrics import deviation_label, deviation_global, auc_label, auc_global, pr_label, pr_global
